b939ad194c8117de3209bf64a77482690a1192dc
Roy Castro Aguilera <rcastro9007@gmail.com>
2019-02-04 23:17:13 -0300

    Fixed/improved minor UI details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'lk-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'Maintenance';
+  title = 'Mantenimiento';
 }
